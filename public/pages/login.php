3601593749e92d0974385a253c1cf64c3af7c610
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-02-19 11:04:00 -0800

    fix font problem

--- public/pages/login.php
+++ public/pages/login.php
@@ -11,12 +11,7 @@ if (isset($_SESSION['uid'])) {
 
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
-    rel="stylesheet" />
-<link href="https://fonts.googleapis.com/css2?family=Knewave&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&display=swap"
-    rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet" />
 
 <link rel="stylesheet" href="../styles/a.css" />
 <link rel="stylesheet" href="../styles/center-center.css" />
